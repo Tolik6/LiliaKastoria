a33a4cfed993abedd02190a80bbd74d02434dd30
Лиля <lilia@example.com>
2026-02-18 11:18:32 +0700

    Начальный коммит: добавлены Kastoria.txt и program.cs

---
+++ program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace LiliaProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Lilia!");
+            Console.WriteLine("Это новый проект");
+        }
+    }
+}
